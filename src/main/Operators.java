03c650fa80e0aa2b97f7a6a488c4328b756fc0ce
Myles <mbrathwaite@academytrainee.com>
2020-11-05 11:44:06 +0000

    Finished Conditional

--- src/main/Operators.java
+++ src/main/Operators.java
@@ -1,5 +1,19 @@
 package main;
 
 public class Operators {
-
+	public double add(double a, double b) {
+		return a + b;
+	}
+	
+	public double subtraction(double a, double b) {
+		return a - b;
+	}
+	
+	public double multiplication(double a, double b) {
+		return a * b;
+	}
+	
+	public double division(double a, double b) {
+		return a / b;
+	}
 }
